ce77794a718b283e2f7d245e426c3f700cfa4df7
Vikramsb <vikrambhambere13@gmail.com>
2014-05-09 16:12:55 -0400

    Ex1

---
+++ Relationaloperator/Relationaloperator/main.m
@@ -0,0 +1,32 @@
+//
+//  main.m
+//  Relationaloperator
+//
+//  Created by User1 on 2014-05-08.
+//  Copyright (c) 2014 therese. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int n, triangularnumber;
+        NSLog(@"table of triangular numbers");
+        NSLog(@"n sum of 1 to n");
+        NSLog(@"---------");
+        
+        triangularnumber =0;
+        
+        for (n=1;n<=10; n++)
+        {
+            triangularnumber +=n;
+            NSLog(@"%i     %i",n,triangularnumber);
+        
+        }
+    }
+    return 0;
+}
+
